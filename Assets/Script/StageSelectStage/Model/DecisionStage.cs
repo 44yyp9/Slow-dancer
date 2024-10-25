d5e75f997d0b962a4918a801a72d7a8aa1158504
44yyp9 <ilovesoccer3939@gmail.com>
2024-10-26 08:27:48 +0900

    [improve] Model Add_LoadScene_Method

--- Assets/Script/StageSelectStage/Model/DecisionStage.cs
+++ Assets/Script/StageSelectStage/Model/DecisionStage.cs
@@ -1,8 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UniRx;
+using UnityEngine.SceneManagement;
 
 public class DecisionStage : MonoBehaviour
 {
-
+    [SerializeField] private PlayerPosition playerPosition;
+    [SerializeField] private Map map;
+    public void loadScene()
+    {
+        var posi = playerPosition.currentStage.Value;
+        var stageObj = map.stages[posi];
+        var _stageObj = stageObj.GetComponent<StageBase>();
+        var stageName = _stageObj.stageName;
+        SceneManager.LoadScene(stageName);
+    }
 }
