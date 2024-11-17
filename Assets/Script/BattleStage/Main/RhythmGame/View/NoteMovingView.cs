e90f14a5df8f8e347995a98be419bc300b01939e
44yyp9 <ilovesoccer3939@gmail.com>
2024-11-17 15:20:11 +0900

    [improve] Add Note Moving

--- Assets/Script/BattleStage/Main/RhythmGame/View/NoteMovingView.cs
+++ Assets/Script/BattleStage/Main/RhythmGame/View/NoteMovingView.cs
@@ -5,5 +5,16 @@ using UniRx;
 
 public class NoteMovingView : MonoBehaviour
 {
+    public ReactiveProperty<Transform> notePosition { get;  set; }
+    [SerializeField] private float speed;
+    private void move(GameObject movingObj)
+    {
+        var _speed=-speed;
+        movingObj.transform.position += new Vector3(_speed, 0, 0) * GameTime.playingTime;
+    }
+    private void Update()
+    {
+        move(gameObject);
+    }
 
 }
--- Assets/Script/BattleStage/Main/State/GameTime.cs
+++ Assets/Script/BattleStage/Main/State/GameTime.cs
@@ -5,5 +5,35 @@ using UniRx;
 
 public class GameTime : MonoBehaviour
 {
-
+    private float time;
+    [SerializeField] private bool isPlaying;
+    public static float playingTime;
+    [SerializeField] private bool isPause;
+    public static float pauseTime;
+    private void Start()
+    {
+        isPlaying = true;
+        isPause=false;
+    }
+    private void Update()
+    {
+        time = Time.deltaTime;
+        //プレイ中ならplayingTimeは0じゃない
+        if (isPlaying) playingTime = time;
+        else playingTime = 0;
+        //ポーズ中ならpauseTimeは0じゃない
+        if(isPause) pauseTime = time;
+        else pauseTime = 0;
+    }
+    //以下のメソッドを提供することでplayかpasuseの切り替えを行う
+    public void playGame()
+    {
+        isPlaying=true;
+        isPause=false;
+    }
+    public void pauseGame()
+    {
+        isPause=true;
+        isPlaying=false;
+    }
 }
