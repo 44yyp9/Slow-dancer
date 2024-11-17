dd1a2fd91e609601431310416cba21424196efe1
44yyp9 <ilovesoccer3939@gmail.com>
2024-11-17 15:19:27 +0900

    [improve] Add Notes Generate

--- Assets/Script/BattleStage/Main/RhythmGame/Presenter/NotesGeneratePresenter.cs
+++ Assets/Script/BattleStage/Main/RhythmGame/Presenter/NotesGeneratePresenter.cs
@@ -5,5 +5,10 @@ using UniRx;
 
 public class NotesGeneratePresenter : MonoBehaviour
 {
-
+    [SerializeField] private ReadingNotes readingNotes;
+    [SerializeField] private NotesGenerateView notesGenerateView;
+    private void Start()
+    {
+        readingNotes.noteTimeList.Subscribe(_ => notesGenerateView.notesData = readingNotes.noteTimeList).AddTo(this);
+    }
 }
--- Assets/Script/BattleStage/Main/RhythmGame/View/NotesGenerateView.cs
+++ Assets/Script/BattleStage/Main/RhythmGame/View/NotesGenerateView.cs
@@ -6,4 +6,40 @@ using UniRx;
 public class NotesGenerateView : MonoBehaviour
 {
 
+    private ReactiveProperty<List<float>> _notesData = new ReactiveProperty<List<float>>();
+    public ReactiveProperty<List<float>> notesData
+    {
+        get { return _notesData; }
+        set
+        {
+            if (value != _notesData)
+            {
+                _notesData = value;
+                StartCoroutine(InstantiateNotes(_notesData.Value));
+            }
+        }
+    }
+    [SerializeField] private GameObject notePrefab;
+    IEnumerator InstantiateNotes(List<float> notes)
+    {
+        for(int i = 0; notes.Count > i; i++)
+        {
+            float delayTime = notes[i];
+            while (delayTime > 0 )
+            {
+                if (GameTime.playingTime == 0)
+                {
+                    delayTime += GameTime.playingTime;
+                }
+                else
+                {
+                    delayTime -= GameTime.playingTime;
+                }
+                yield return null;
+            }
+            yield return new WaitForSeconds(delayTime);
+            Vector3 generatePosi=gameObject.transform.position;
+            Instantiate(notePrefab, generatePosi, Quaternion.identity,gameObject.transform);
+        }
+    }
 }
