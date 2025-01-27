6e497c16e047e28822a74309b0c3c0539beae7a7
SG <ilovesoccer3939@gmail.com>
2025-01-27 20:09:10 +0900

    [Update] Add Mouse Hover Method

--- Assets/Script/StartScene/View/GameStartingView.cs
+++ Assets/Script/StartScene/View/GameStartingView.cs
@@ -2,8 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UniRx;
+using UnityEngine.EventSystems;
 
 public class GameStartingView : UIViewTemplate
 {
+    public override void OnEnterMouseButton(PointerEventData eventData)
+    {
+        Debug.Log("スタートボタンに侵入しました");
+    }
+    public override void OnExitMouseButton(PointerEventData eventData)
+    {
+        Debug.Log("スタートボタンから出ていきました");
+    }
 
 }
